170126f6e83717af25f08b415eac9d8b59039b4b
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-24 13:46:26 +0100

    Working env var load and panic on missing

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A thin logic layer over the [Clubhouse API](https://clubhouse.io/api/rest/v2/), clubhouse-bouncer acts as a proxy for complex requests.
 
+## Environment variables
+
+For required environment variables, see `.env.example`. The app will panic if these are not present.
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+fn env_var_required(key: &str) -> String {
+    env::var(key).expect(&format!("Missing env var {}", key))
+}
+
 fn main() {
+    let cluhouse_api_token: String = env_var_required("CLUBHOUSE_API_TOKEN");
+    let bouncer_credentials: String = env_var_required("BOUNCER_CREDENTIALS");
+
+    println!("{}", &bouncer_credentials);
     println!("Hello, world!");
 }
