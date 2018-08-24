0dce3295cdb736b47568969d298f3b08a07566f3
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-24 17:06:29 +0100

    config and secrets passed down to workers

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 extern crate futures;
 extern crate hyper;
+extern crate hyper_tls;
 #[macro_use]
 extern crate log;
 extern crate pretty_env_logger;
@@ -14,8 +15,11 @@ use futures::{future, Future, Stream};
 use hyper::{Body, Client, Method, Request, Response, Server, StatusCode, header};
 use hyper::client::HttpConnector;
 use hyper::service::service_fn;
+use hyper_tls::{HttpsConnector};
 
 static NOTFOUND: &[u8] = b"Not Found";
+static CLUBHOUSE_URL_WORKFLOWS: &str = "https://api.clubhouse.io/api/v2/workflows?token={token}";
+static CLUBHOUSE_URL_STORY: &str = "http://127.0.0.1:1337/web_api";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct QueryDeployable {
@@ -27,22 +31,26 @@ fn env_var_required(key: &str) -> String {
     env::var(key).expect(&format!("Missing env var {}", key))
 }
 
-fn response_examples(req: Request<Body>, client: &Client<HttpConnector>)
+fn response_examples(req: Request<Body>, client: &Client<HttpsConnector<HttpConnector>>, config: &BouncerConfig)
     -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>
 {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/deployable") => {
+
+            warn!("{}", &config.bouncer_credentials);
 
             req.into_body().concat2().and_then(|stream| {
                 let body = String::from_utf8(stream.to_vec()).expect("Invalid string body");
                 warn!("Got body; {}", &body);
                 let q: QueryDeployable = serde_json::from_str(&body).expect("Invalid tickets JSON");
                 let a: String = serde_json::to_string(&q).expect("Error serializing to JSON");
+
                 future::ok(Response::builder()
                     .header(header::CONTENT_TYPE, "application/json")
                     .body(Body::from(a))
                     .unwrap()
                 )
+
             }).boxed()
         }
         _ => {
@@ -56,24 +64,37 @@ fn response_examples(req: Request<Body>, client: &Client<HttpConnector>)
     }
 }
 
+#[derive(Debug, Clone)]
+struct BouncerConfig {
+    bouncer_credentials: String,
+    clubhouse_api_token: String
+}
+
 fn main() {
     pretty_env_logger::init();
 
-    let cluhouse_api_token: String = env_var_required("CLUBHOUSE_API_TOKEN");
-    let bouncer_credentials: String = env_var_required("BOUNCER_CREDENTIALS");
     let port: String = env::var("PORT").unwrap_or(String::from("2686"));
-
     let addr = format!("127.0.0.1:{}", &port).parse().unwrap();
 
+    // Calculate static config to pass down to workers
+    let bouncer_config = BouncerConfig {
+        bouncer_credentials: env_var_required("BOUNCER_CREDENTIALS"),
+        clubhouse_api_token: env_var_required("CLUBHOUSE_API_TOKEN")
+    };
+
     hyper::rt::run(future::lazy(move || {
+        // 4 is number of blocking threads for DNS
+        let https = HttpsConnector::new(4).unwrap();
         // Share a `Client` with all `Service`s
-        let client = Client::new();
+        let client = Client::builder()
+            .build::<_, hyper::Body>(https);
 
         let new_service = move || {
             // Move a clone of `client` into the `service_fn`.
             let client = client.clone();
+            let bouncer_config = bouncer_config.clone();
             service_fn(move |req| {
-                response_examples(req, &client)
+                response_examples(req, &client, &bouncer_config)
             })
         };
 
