7a308d019e638a089e3cd5260a012d803ee02dee
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-24 14:22:26 +0100

    H=Working hyper route 404 and logging

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ A thin logic layer over the [Clubhouse API](https://clubhouse.io/api/rest/v2/), 
 
 For required environment variables, see `.env.example`. The app will panic if these are not present.
 
+## Development
+
+During development, you may wish to set
+
+```
+export RUST_LOG=bouncer=trace
+```
+
+to see logs.
+
+Hot reoading is reccommended via:
+
+```
+cargo install cargo-watch
+cargo watch -x run
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,67 @@
 use std::env;
 
+extern crate futures;
+extern crate hyper;
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+extern crate serde_json;
+
+use futures::{future, Future, Stream};
+
+use hyper::{Body, Chunk, Client, Method, Request, Response, Server, StatusCode, header};
+use hyper::client::HttpConnector;
+use hyper::service::service_fn;
+
+static NOTFOUND: &[u8] = b"Not Found";
+
 fn env_var_required(key: &str) -> String {
+    trace!("Loading env var {}", key);
     env::var(key).expect(&format!("Missing env var {}", key))
 }
 
+fn response_examples(req: Request<Body>, client: &Client<HttpConnector>)
+    -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>
+{
+    match (req.method(), req.uri().path()) {
+        _ => {
+            // Return 404 not found response.
+            let body = Body::from(NOTFOUND);
+            Box::new(future::ok(Response::builder()
+                                         .status(StatusCode::NOT_FOUND)
+                                         .body(body)
+                                         .unwrap()))
+        }
+    }
+}
+
 fn main() {
+    pretty_env_logger::init();
+
     let cluhouse_api_token: String = env_var_required("CLUBHOUSE_API_TOKEN");
     let bouncer_credentials: String = env_var_required("BOUNCER_CREDENTIALS");
+    let port: String = env::var("PORT").unwrap_or(String::from("2686"));
 
-    println!("{}", &bouncer_credentials);
-    println!("Hello, world!");
+    let addr = format!("127.0.0.1:{}", &port).parse().unwrap();
+
+    hyper::rt::run(future::lazy(move || {
+        // Share a `Client` with all `Service`s
+        let client = Client::new();
+
+        let new_service = move || {
+            // Move a clone of `client` into the `service_fn`.
+            let client = client.clone();
+            service_fn(move |req| {
+                response_examples(req, &client)
+            })
+        };
+
+        let server = Server::bind(&addr)
+            .serve(new_service)
+            .map_err(|e| error!("server error: {}", e));
+
+        info!("clubhouse-bouncer ready on http://{}", addr);
+
+        server
+    }));
 }
