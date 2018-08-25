cdc9274ef2a4e5be18a2032d38e97d747b205f45
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-25 13:48:36 +0100

    Updated json parsing, url generation and params

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM frolvlad/alpine-rust as builder
 
+RUN apk add --no-cache libressl libressl-dev
+
 # cache our dependencies
 RUN USER=root cargo new --bin repo
 WORKDIR /repo
@@ -14,8 +16,9 @@ RUN cargo build --release
 # runtime image
 FROM alpine:3.8
 # required to allow stacktrace/panic recovery
-RUN apk add --no-cache libgcc
+RUN apk add --no-cache libgcc libressl
 
 COPY --from=builder /repo/target/release/bouncer .
+ENV RUST_LOG=bouncer=info
 CMD ["./bouncer"]
 
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate url;
 
 use futures::{future, Future, Stream};
 
@@ -16,6 +17,8 @@ use hyper::client::HttpConnector;
 use hyper::service::service_fn;
 use hyper::{header, Body, Client, Method, Request, Response, Server, StatusCode};
 use hyper_tls::HttpsConnector;
+
+use url::Url;
 
 static NOTFOUND: &[u8] = b"Not Found";
 
@@ -38,6 +41,13 @@ struct Workflow {
     states: Vec<WorkflowState>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Story {
+    id: u64,
+    name: String,
+    workflow_state_id: u64,
+}
+
 fn env_var_required(key: &str) -> String {
     trace!("Loading env var {}", key);
     env::var(key).expect(&format!("Missing env var {}", key))
@@ -50,29 +60,47 @@ fn response_examples(
 ) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/deployable") => {
-            let q = req.into_body().concat2().and_then(|stream| {
+            let get_story_ids = req.into_body().concat2().and_then(|stream| {
                 let q: QueryDeployable =
                     serde_json::from_slice(&stream).expect("Invalid tickets JSON");
                 warn!("{:?}", q);
-                future::ok(q)
+                future::ok(q.story_ids)
             });
 
+            let mut url_workflows =
+                Url::parse("https://api.clubhouse.io/api/v2/workflows").unwrap();
+            url_workflows
+                .query_pairs_mut()
+                .append_pair("token", &config.clubhouse_api_token);
+
             let get_workflows = client
-                .get(
-                    format!(
-                        "https://api.clubhouse.io/api/v2/workflows?token={token}",
-                        token = config.clubhouse_api_token
-                    ).parse()
-                        .unwrap(),
-                )
+                .get(url_workflows.as_str().parse().unwrap())
                 .and_then(|res| {
-                    trace!("clubhouse esponse: {}", res.status());
+                    trace!("clubhouse response: {}", res.status());
                     res.into_body().concat2().and_then(|stream| {
                         future::ok(serde_json::from_slice::<Vec<Workflow>>(&stream).unwrap())
                     })
                 });
 
-            let res = get_workflows.and_then(|s| {
+            let mut url_stories = Url::parse("https://api.clubhouse.io/api/v2/stories/")
+                .unwrap()
+                .join("9812")
+                .unwrap();
+            url_stories
+                .query_pairs_mut()
+                .append_pair("token", &config.clubhouse_api_token);
+            info!("{}", url_stories.as_str());
+
+            let get_stories = client
+                .get(url_stories.as_str().parse().unwrap())
+                .and_then(|res| {
+                    trace!("clubhouse response: {}", res.status());
+                    res.into_body().concat2().and_then(|stream| {
+                        future::ok(serde_json::from_slice::<Story>(&stream).unwrap())
+                    })
+                });
+
+            let res = get_stories.and_then(|s| {
                 let b = serde_json::to_string(&s).unwrap();
                 future::ok(
                     Response::builder()
@@ -132,7 +160,7 @@ fn main() {
             .serve(new_service)
             .map_err(|e| error!("server error: {}", e));
 
-        info!("clubhouse-bouncer ready on http://{}", addr);
+        warn!("clubhouse-bouncer ready on http://{}", addr);
 
         server
     }));
