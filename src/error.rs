980bc470596a6361dc1350ff4562b75c6a51f28f
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-27 13:06:00 +0100

    scripts permissions

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+extern crate hyper;
+extern crate serde_json;
+
+// Define a type so we can return multile types of errors
+pub enum FetchError {
+    Http(hyper::Error),
+    Json(serde_json::Error),
+}
+
+impl From<hyper::Error> for FetchError {
+    fn from(err: hyper::Error) -> FetchError {
+        FetchError::Http(err)
+    }
+}
+
+impl From<serde_json::Error> for FetchError {
+    fn from(err: serde_json::Error) -> FetchError {
+        FetchError::Json(err)
+    }
+}
